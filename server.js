f79f05a7ce3c6be7078ef005ea75990b7058d882
sorayapnblm <sorayapanambalom@gmail.com>
2023-08-04 21:10:41 +0400

    import the http module and set up the server with it

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 // import path module
 const path = require('path');
+// import http module
+const http = require('http');
 // import express;js framework
 const express = require('express');
 
+const socketio = require('socket.io');
+
 //create an instance of the Express application
 const app = express();
+//create an HTTP server instance using express as a request listener
+const server = http.createServer(app);
 
 // Set staic folder
 app.use(express.static(path.join(__dirname, 'html-scss-css-js')));
@@ -12,6 +18,6 @@ app.use(express.static(path.join(__dirname, 'html-scss-css-js')));
 // initialize the port
 const PORT = 3000 || 3000;
 
-// instruct the Express application to start listening on the specified port
+// instruct the Express application to start listening on the specified port for incoming http requests
 // when the server starts successfully, it will log a message
-app.listen(PORT, () => console.log(`Server running on port  ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port  ${PORT}`));
