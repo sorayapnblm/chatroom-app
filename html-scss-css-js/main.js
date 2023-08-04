b8ba8538609c0e905a4de6456513c9cc33eed183
sorayapnblm <sorayapanambalom@gmail.com>
2023-08-04 22:08:39 +0400

    Add a way to handle user input

--- html-scss-css-js/main.js
+++ html-scss-css-js/main.js
@@ -1,3 +1,6 @@
+// Get a reference to the 'container-chatroom-form' in the chat.html to allow the display of submitted message 
+const chatForm = document.getElementById('container-chatroom-form');
+
 // establish a connection to the server using socket.io
 const socket =io();
 
@@ -6,4 +9,16 @@ const socket =io();
 //  In this case, it logs the received message to the console.
 socket.on('message', message => {
     console.log(message);
+})
+
+//message submit
+chatForm.addEventListener('submit', (e) => {
+    // P the default form submission behavior, which would cause the page to reload.
+    e.preventDefault();
+
+    //get the value of the input field
+    const msg = e.target.elements.msg.value;
+
+    // log the message content to the console.
+    console.log(msg);
 })
