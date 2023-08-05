488cec7cbc159e1d44870801401d29ac5e215332
sorayapnblm <sorayapanambalom@gmail.com>
2023-08-05 18:48:14 +0400

    Add comments

--- utils/messages.js
+++ utils/messages.js
@@ -1,5 +1,7 @@
 const moment = require('moment');
 
+
+// This function is used to format messages with timestamps and other details. This enhances the readability and user experience of your chat application.
 function formatMessage(username, text) {
     // Return an objet
     return {
